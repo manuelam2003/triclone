6d075696685284f6047115781d3bb6c19b13058d
Manuel Alvarez <manuel.am2003@gmail.com>
2024-09-12 11:42:51 +0200

    added rate limit middleware2

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -24,44 +24,33 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Define a client struct to hold the rate limiter and last seen time for each
-	// client.
 	type client struct {
 		limiter  *rate.Limiter
 		lastSeen time.Time
 	}
 
 	var (
-		mu sync.Mutex
-		// Update the map so the values are pointers to a client struct.
+		mu      sync.Mutex
 		clients = make(map[string]*client)
 	)
 
-	// Launch a background goroutine which removes old entries from the clients map once
-	// every minute.
 	go func() {
 		for {
 			time.Sleep(time.Minute)
 
-			// Lock the mutex to prevent any rate limiter checks from happening while
-			// the cleanup is taking place.
 			mu.Lock()
 
-			// Loop through all clients. If they haven't been seen within the last three
-			// minutes, delete the corresponding entry from the map.
 			for ip, client := range clients {
 				if time.Since(client.lastSeen) > 3*time.Minute {
 					delete(clients, ip)
 				}
 			}
 
-			// Importantly, unlock the mutex when the cleanup is complete.
 			mu.Unlock()
 		}
 	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Only carry out the check if rate limiting is enabled.
 		if app.config.limiter.enabled {
 			ip, _, err := net.SplitHostPort(r.RemoteAddr)
 			if err != nil {
@@ -73,8 +62,6 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 			if _, found := clients[ip]; !found {
 				clients[ip] = &client{
-					// Use the requests-per-second and burst values from the config
-					// struct.
 					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
 				}
 			}
